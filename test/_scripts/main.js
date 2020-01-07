6994c1458df2f6c0b39f4d588575161440c1723a
Nuno Sequeira <nuno.sequeira@parceiros.nos.pt>
2020-01-07 11:25:31 +0000

    Updated banner

--- test/_scripts/main.js
+++ test/_scripts/main.js
@@ -4,10 +4,16 @@ var forEach = function (array, callback, scope) {
   }
 };
 
+var isMobile = function (el) {
+  // check if element is visible
+  return el.offsetParent === null;
+};
+
 var banners = document.querySelectorAll('.o-banner-gallery-wrapper');
 
 forEach(banners, function (index, value) {
   var gallery = banners[index].querySelector('.o-banner-gallery');
+  var controls = banners[index].querySelector('.tns-controls');
   var delay;
 
   var slider = tns({
@@ -19,45 +25,47 @@ forEach(banners, function (index, value) {
     autoplayHoverPause: false,
     speed: 1000,
     controlsPosition: 'bottom',
-    animateIn: 'jello',
-    animateOut: 'rollOut',
     navPosition: 'bottom',
     navContainer: banners[index].querySelector('.tns-nav'),
     controls: false,
-    autoplayButtonOutput: false
+    autoplayButtonOutput: false,
+    swipeAngle: 40,
+    preventScrollOnTouch: 'auto'
   });
 
-  var wrapperELem = slider.getInfo().container.parentNode.parentNode.parentNode.parentNode;
+  if (!isMobile(controls)) {
+    var wrapperELem = slider.getInfo().container.parentNode.parentNode.parentNode.parentNode;
 
-  wrapperELem.onmouseenter  = function () {
-    slider.pause();
-    clearTimeout(delay);
-  };
-
-  wrapperELem.onmouseleave = function () {
-    var time;
-    var timeout;
-    clearTimeout(delay);
-    time = slider.getInfo().navContainer.querySelector('.tns-nav-active span').offsetWidth * 5000 / slider.getInfo().navContainer.querySelector('.tns-nav-active').offsetWidth;
-    timeout = 5000 - time;
-    delay = setTimeout(function () {
-      slider.goTo('next');
-      slider.play();
+    wrapperELem.onmouseenter = function () {
+      slider.pause();
       clearTimeout(delay);
-    }, timeout);
-  };
+    };
 
-  wrapperELem.querySelector('.prev').addEventListener('click', function (event) {
-    event.preventDefault();
-    clearTimeout(delay);
-    slider.pause();
-    slider.goTo('prev');
-  });
+    wrapperELem.onmouseleave = function () {
+      var time;
+      var timeout;
+      clearTimeout(delay);
+      time = slider.getInfo().navContainer.querySelector('.tns-nav-active span').offsetWidth * 5000 / slider.getInfo().navContainer.querySelector('.tns-nav-active').offsetWidth;
+      timeout = 5000 - time;
+      delay = setTimeout(function () {
+        slider.goTo('next');
+        slider.play();
+        clearTimeout(delay);
+      }, timeout);
+    };
 
-  wrapperELem.querySelector('.next').addEventListener('click', function (event) {
-    event.preventDefault();
-    clearTimeout(delay);
-    slider.pause();
-    slider.goTo('next');
-  });
+    wrapperELem.querySelector('.prev').addEventListener('click', function (event) {
+      event.preventDefault();
+      clearTimeout(delay);
+      slider.pause();
+      slider.goTo('prev');
+    });
+
+    wrapperELem.querySelector('.next').addEventListener('click', function (event) {
+      event.preventDefault();
+      clearTimeout(delay);
+      slider.pause();
+      slider.goTo('next');
+    });
+  }
 });
